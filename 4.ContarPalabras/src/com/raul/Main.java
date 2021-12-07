300d5dac4ff66a2ab9e50440e5f4c79f5bd444a8
TheLordRalph <raulete10m@gmail.com>
2021-12-07 16:48:42 +0100

    Ejercicion 4.ContarPalabras ultimo commit

--- 4.ContarPalabras/src/com/raul/Main.java
+++ 4.ContarPalabras/src/com/raul/Main.java
@@ -12,20 +12,19 @@ public class Main {
     public static void main(String[] args) {
 
         var path = Paths.get(System.getProperty("user.dir"), "Files");
+        // "path" almacena la ruta donde se encuntran los archivos que queremos contar las palabras
+        // en este caso yo los tengo en una carpeta Files.
 
         try {
             DirectoryStream<Path> Path1 = Files.newDirectoryStream(path);
-            for (Path file : Path1) {
-                WordCounter wordCounter = new WordCounter(file);
+            // Con el siguiente foreach guardo en "pathOfFile" la ruta de los fichero que estan dentro de Files
+            for (Path pathOfFile : Path1) {
+                // Se crea una clase "WordCounte" a la que le pasamos el path del primer archivo y contamos sus palabras.
+                WordCounter wordCounter = new WordCounter(pathOfFile);
                 wordCounter.counter();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-        //path.get(System.getProperty("user.dir"));
-        //List<File> libros;
-
     }
 }
--- 4.ContarPalabras/src/com/raul/WordCounter.java
+++ 4.ContarPalabras/src/com/raul/WordCounter.java
@@ -3,28 +3,32 @@ package com.raul;
 import java.io.*;
 import java.nio.file.Path;
 
-public class WordCounter {
+public class WordCounter extends Thread {
 
+    // ATRIBUTOS
     private Path _path;
+    private String[] _wordsInLine;
+    private int _allWors;
 
+    // CONSTRUCTOR
     public WordCounter(Path path) throws IOException {
         _path = path;
     }
 
+    // METODOS
     public void counter () throws IOException {
 
         File _file = new File(String.valueOf(_path));
         var fileReader = new FileReader(_file);
         var bufferedReader = new BufferedReader(fileReader);
         var line = "";
-        String[] words = new String[]{};
-        int allWors = 0;
+        _allWors = 0;
 
         while ((line = bufferedReader.readLine()) != null) {
-            words = line.split(" ");
-            allWors += words.length;
+            _wordsInLine = line.split(" ");
+            _allWors += _wordsInLine.length;
         }
         bufferedReader.close();
-        System.out.printf("File %s has %d words %n", _path.getFileName(), allWors);
+        System.out.printf("File %s has %d words %n", _path.getFileName(), _allWors);
     }
 }
